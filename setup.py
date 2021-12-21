07c9098f4453feb1a3d0d52219eec31119b5485b
Mayur Borkar <borkarm112@gmail.com>
2021-12-21 14:47:19 +0530

    setup.py file add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cement-Strength-Prediction",
+    version="0.0.3",
+    description="ML project",
+    author="Mayur Borkar",
+    packages=find_packages(),
+    license="MIT"
+)
